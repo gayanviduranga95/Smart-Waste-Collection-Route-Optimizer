a35138582c1a24485d9ebc5fbd5c489e07e53943
Gayan Viduranga <gayanviduranga95@gmail.com>
2026-04-28 22:38:54 +0530

    addRoad function

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -3,3 +3,7 @@
 void Graph::addLocation(string name){
     adjList[name];
 }
+void Graph::addRoad(string from, string to,int distance){
+    adjList[from].push_back({to, distance});
+    adjList[to].push_back({from, distance}); 
+}
